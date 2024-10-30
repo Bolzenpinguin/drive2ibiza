71915045344414d3fc66129940446c773a62007a
Kurt <schubert.geschaeftlich@gmail.com>
2024-10-30 09:46:07 +0100

    Revert "Added Colors in colors.dart and createt foldors for images and icons and include them int the pubspec.yaml file"
    This reverts commit 6ce74ae31135faed28cdc8e07b637ebf96b21969.
    
    Zurück

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -17,18 +17,3 @@ const Color highlightLabel = Color.fromRGBO(0, 122, 255, 1);
 
 // Backgrounds
 const Color appBackgroundColor = Color.fromRGBO(242, 242, 247, 1); // background grey
-
-// Driver Colors
-const Color driverOne = Color.fromRGBO(0, 0, 0, 1);
-const Color driverTwo = Color.fromRGBO(0, 122, 255, 1);
-const Color driverThree = Color.fromRGBO(52, 199, 89, 1);
-const Color driverFour = Color.fromRGBO(255, 204, 0, 1);
-const Color driverFive = Color.fromRGBO(175, 82, 222, 1);
-const Color diverSix = Color.fromRGBO(122, 198, 245, 1);
-const Color driverSeven = Color.fromRGBO(255, 59, 48, 1);
-const Color driverEight = Color.fromRGBO(88, 86, 214, 1);
-const Color diverNine = Color.fromRGBO(162, 132, 94, 1);
-const Color diverTen = Color.fromRGBO(48, 176, 199, 1);
-
-// Extra Colors
-const Color addBTNColor = Color.fromRGBO(206, 206, 206, 1); // grey + BTN Main Color
