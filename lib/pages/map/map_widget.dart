923f039d9680bc9db0fdf54add507cdff1933b67
Kurt <schubert.geschaeftlich@gmail.com>
2024-11-08 00:00:24 +0100

    add searchbar in maps_widget.dart but without function

--- lib/pages/map/map_widget.dart
+++ lib/pages/map/map_widget.dart
@@ -30,7 +30,6 @@ class _MapWidgetState extends State<MapWidget> {
           ),
         ],
       ),
-
       child: Stack(
         children: [
           ClipRRect(
@@ -50,11 +49,10 @@ class _MapWidgetState extends State<MapWidget> {
                     Marker(
                       point: lastPostion,
                       width: 80.0,
-                      height: 80.0,
+                      height: 200.0,
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
                         children: [
-                          // Big Circle
                           Container(
                             width: 50.0,
                             height: 50.0,
@@ -70,7 +68,6 @@ class _MapWidgetState extends State<MapWidget> {
                               ),
                             ),
                           ),
-                          // Small Circle
                           Container(
                             width: 10.0,
                             height: 10.0,
@@ -87,46 +84,99 @@ class _MapWidgetState extends State<MapWidget> {
               ],
             ),
           ),
-
-          // Reset View Button
           Positioned(
             top: 10.0,
             right: 10.0,
             child: FloatingActionButton(
-              onPressed: resetView,
+              onPressed: () {
+                mapController.move(lastPostion, 17.5);
+              },
               backgroundColor: Colors.white,
               mini: true,
-              child:
-                  SvgPicture.asset(
-                    'assets/icon/topCar.svg',
-                    fit: BoxFit.contain,
-                    colorFilter: ColorFilter.mode(appPrimaryColor, BlendMode.srcIn),
-                  ),
+              child: SvgPicture.asset(
+                'assets/icon/topCar.svg',
+                fit: BoxFit.contain,
+                colorFilter: ColorFilter.mode(appPrimaryColor, BlendMode.srcIn),
+              ),
             ),
           ),
-
           Positioned(
             top: 60.0,
             right: 10.0,
             child: FloatingActionButton(
-              onPressed: getCurrentLocation,
-                backgroundColor: Colors.white,
-                mini: true,
-                child:
-                  Icon(Icons.gps_fixed, color: appPrimaryColor,),
-            )
-          )
+              onPressed: () {
+                // TODO: Geopostion bekommen
+                print('BTN for Geolocation pressed');
+              },
+              backgroundColor: Colors.white,
+              mini: true,
+              child: Icon(Icons.gps_fixed, color: appPrimaryColor),
+            ),
+          ),
+
+
+          // Search Bar and Login Button at the Bottom
+          Positioned(
+            bottom: 0.0,
+            left: 0.0,
+            right: 0.0,
+            child: Container(
+              padding: const EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    blurRadius: 10,
+                    offset: Offset(0, 5),
+                  ),
+                ],
+              ),
+              child: Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.location_on_outlined,
+                        color: appSecondaryColor,
+                      ),
+                      border: OutlineInputBorder(),
+                      labelText: 'Adresse',
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: appUnselectedIconColor,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: appUnselectedIconColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  SizedBox(
+                    width: settingsBoxWidth,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // TODO: Noch speichern der Position hinzufügen
+                        print('Location Speicher BTN gedrückt');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(vertical: paddingNormal),
+                        foregroundColor: btnFontColor,
+                        backgroundColor: btnBackgroundColor,
+                      ),
+                      child: const Text('Save Postion'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
   }
-
-  void resetView() {
-    mapController.move(lastPostion, 17.5);
-  }
-}
-
-void getCurrentLocation() {
-  // TODO: Geopostion bekommen
-  print('BTN for Geolocation pressed');
 }
